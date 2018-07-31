ba55c7a74b0c1d78c6cb34704f373dae52a5cc44
hackyhacker <hackyhacker@byom.com>
2018-07-31 14:30:39 +0200

    my first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deepShit',
+      version='0.1',
+      description='',
+      url='https://github.com/hackyhacker/deepShit.git',
+      packages=['deepShit'],
+      zip_safe=False,
+      install_requires=[''],
+      test_suite='py.test',
+      tests_require=['pytest'])
